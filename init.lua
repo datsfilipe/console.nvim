d70bf6de7565a91202510af9c63b43604ec54cc9
Filipe Lima <datsfilipe.foss@proton.me>
2025-12-22 23:07:40 -0300

    feat: add dev init file

---
+++ init.lua
@@ -0,0 +1,14 @@
+vim.cmd [[set runtimepath=$VIMRUNTIME]]
+
+local current_dir = vim.fn.getcwd()
+vim.opt.rtp:prepend(current_dir)
+
+local status, plugin = pcall(require, 'console')
+if not status then
+  vim.print 'shit happens'
+else
+  plugin.setup {
+    command_name = 'TestRun',
+    grep_command_name = 'TestGrep',
+  }
+end
